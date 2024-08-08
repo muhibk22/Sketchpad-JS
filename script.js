4833677b4148beecfdf7d45fc555a73b07e41a6e
Muhib Khan <m.muhibk22@gmail.com>
2024-08-09 04:46:17 +0500

    add slider to control number of divs genereted

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const pen=document.querySelector("#pen");
 const pencil=document.querySelector("#pencil");
 const eraser=document.getElementById("eraser");
 const rainbow=document.getElementById("rainbow");
+const slider= document.getElementById("slider");
 
 let currentColor="#000000"
 let isMouseDown = false
 let tool="pen";
+let size=16;
 document.body.onmousedown = () => (isMouseDown = true)
 document.body.onmouseup = () => (isMouseDown=false)
 
@@ -45,6 +47,14 @@ function draw(event){
     else if (tool=="eraser"){
         event.target.style.opacity=0;
     }
+    else if (tool=="rainbow"){
+        let red,green,blue;
+        red=Math.floor(Math.random()*256);
+        green=Math.floor(Math.random()*256);
+        blue=Math.floor(Math.random()*256);
+        event.target.style.opacity=1;
+        event.target.style.backgroundColor=`rgb(${red},${green},${blue})`;
+    }
 }
 
 pen.addEventListener('click', function(){
@@ -59,6 +69,18 @@ eraser.addEventListener('click', function(){
     tool="eraser";
 });
 
+rainbow.addEventListener('click', function(){
+    tool="rainbow";
+})
+
+slider.onchange = (e) => setSize(e.target.value);
+
+function setSize(target){
+    container.innerHTML="";
+    size=16+(100-target);
+    createDivs(size);
+}
+
 function updateColor() {
     palette.style.backgroundColor = colorPicker.value;
     currentColor=colorPicker.value;
@@ -71,4 +93,4 @@ palette.addEventListener('click', function() {
 });
 
 
-createDivs(26);
+createDivs(size);
